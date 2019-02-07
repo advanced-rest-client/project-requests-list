07e9a188f8f20de2328cae8585b3e8d4cee973de
Pawel <jarrodek@gmail.com>
2019-02-06 20:08:16 -0800

    New: Adding test coverage

--- demo/test-data-generator.js
+++ demo/test-data-generator.js
@@ -3,8 +3,8 @@ const methods = ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'HEAD'];
 const methodsSize = methods.length - 1;
 const DataGenerator = {};
 DataGenerator.genRequestObject = function() {
-  var methodIndex = chance.integer({min: 0, max: methodsSize});
-  var obj = {
+  const methodIndex = chance.integer({min: 0, max: methodsSize});
+  const obj = {
     _id: chance.string({length: 5}),
     name: chance.sentence({words: 2}),
     method: methods[methodIndex],
@@ -14,8 +14,8 @@ DataGenerator.genRequestObject = function() {
 };
 DataGenerator.generateRequests = function(size) {
   size = size || 25;
-  var result = [];
-  for (var i = 0; i < size; i++) {
+  const result = [];
+  for (let i = 0; i < size; i++) {
     result.push(DataGenerator.genRequestObject());
   }
   return result;
